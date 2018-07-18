09781afea727256bc7aa2f02d07ad706f3487712
Matthew Brown <lokislayer@gmail.com>
2018-07-17 21:22:27 -0500

    Python prime number example problem

--- ex11.py
+++ ex11.py
@@ -1,3 +1,7 @@
+# Determine Prime Number
+# There is a more elegant way of doing it but this version is simply by getting it divisors
+# because if it has more than 1 divisor than it is not prime since prime has to have only 1
+# which itself(I am ignoring 1 since it is a divisor in itself)
 def inputNumber(message):
     while True:
         try:
@@ -9,6 +13,18 @@ def inputNumber(message):
             return userInput
 
 def isPrime(number):
-    pass
+    if (number == 1):
+        return True
+    else:
+        a = []
+        for x in range(2, number + 1):
+            if (number % x == 0):
+                a.append(x)
+        # If the length is greater than 0 then it is not prime
+        return len(a) == 1
 
-prime = inputNumber("Enter a number ")    
+prime = inputNumber("Enter a number to see if it prime or not: ")
+if (isPrime(prime)):
+    print("The number " + str(prime) + " is a prime number")
+else:
+    print("The number " + str(prime) + " is not a prime number")    
